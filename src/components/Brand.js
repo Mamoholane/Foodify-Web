1a928514f0a59b1cfdcee9e354e1d6a2ceffc9da
Mamoholane <mamoholanemogotlane@gmail.com>
2025-01-25 21:06:17 +0200

    added code for Brand component

---
+++ src/components/Brand.js
@@ -0,0 +1,18 @@
+
+const Brand = () => {
+  return (
+    <div className="header">
+      <div className="brand">
+        <div className="logo">
+          <img src="/logo.jpg"/>
+        </div>
+        <div className="slogan">
+        <h1>Foodify</h1>
+        <h4>Unleashing the Chef in You!!</h4></div>
+      </div>
+    </div>
+);
+  
+};
+
+export default Brand;
